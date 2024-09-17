3b6d7e2e225fe3af7a47c50742155504298690e5
Owyn Wendello <owynwend@qu-j5.nov.21-school.ru>
2024-09-17 17:13:46 +0300

    firs result

--- src/src/frontend.c
+++ src/src/frontend.c
@@ -8,7 +8,7 @@ bool print_game_field(Game_t game) {
   if (game.current_field.field != NULL) {
     for (int i = 0; i < ROWS_FIELD; i++) {
       for (int j = 0; j < COLS_FIELD; j++) {
-        if (game.current_field.field[i][j] == 0) mvprintw(i + 1, j * 2 + 2, "_");
+        if (game.current_field.field[i][j] == 0) mvprintw(i + 1, j * 2 + 2, "__");
         if (game.current_field.field[i][j] == 1) mvprintw(i + 1, j * 2 + 2, "[]");
       }
     }
